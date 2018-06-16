13a4e196c164358ba1eef05537c4ebb3b9d157cc
Inés Pederiva <ines.pederiva@gmail.com>
2018-06-15 22:19:09 -0300

    Final version

--- src/main/java/entrega/cai/PlayStationApplicationCollector.java
+++ src/main/java/entrega/cai/PlayStationApplicationCollector.java
@@ -26,39 +26,5 @@ public class PlayStationApplicationCollector extends ApplicationCollector {
 	public String getUrl(Element headline){
 		return headline.select("a").attr("href");
 	}
-	/*
-	 * A partir de la url pasada como parámetro hace pageCount iteraciones con
-	 * el fin de recolectar cada una de las páginas de las aplicaciones sobre
-	 * las que tiene que ir a buscar el detalle para guardar en la base de
-	 * datos.
-	 * 
-	 * @see entrega.cai.ApplicationCollector#collect(java.lang.String)
-	 */
-	// public List<ApplicationPageAnalyser> collect(String rootUrl) {
-	//
-	// List<ApplicationPageAnalyser> analysers = new
-	// ArrayList<ApplicationPageAnalyser>();
-	// Document doc;
-	// System.out
-	// .println("Se comienza la búsqueda de las páginas comenzando con: "
-	// + rootUrl);
-	// for (int i = 1; i < getPageCount(); i++) {
-	// try {
-	// doc = Jsoup.connect(rootUrl + i).get();
-	// Elements clusterDetailsTitle = doc
-	// .select(".grid-cell-container .grid-cell-row .grid-cell--game");
-	// for (Element headline : clusterDetailsTitle) {
-	// analysers.add(new PSPageAnalyser(
-	// "https://store.playstation.com"
-	// + headline.select("a").attr("href")));
-	// }
-	// System.out.println(String.format(
-	// "Se procesó la página %s de PlayStation Store", i));
-	// } catch (IOException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// return analysers;
-	// }
 
 }
